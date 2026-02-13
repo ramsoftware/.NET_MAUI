a3645ec74f6519a081b2ef877b8b0a841aa9af24
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2026-02-13 15:59:58 -0500

    Add files via upload

---
+++ 27/DibujarFiguras.cs
@@ -0,0 +1,35 @@
+﻿namespace Graficos {
+    public class DibujarFiguras : IDrawable {
+
+        public void Draw(ICanvas Lienzo, RectF dirtyRect) {
+
+            //Grosor de la Línea
+            Lienzo.StrokeSize = 2;
+
+
+            //Color de la Línea
+            Lienzo.StrokeColor = Colors.Red;
+            int contador = 10;
+            int incremento = 2;
+
+            do {
+                Lienzo.DrawLine(0, contador, dirtyRect.Height/2, contador);
+                incremento++;
+                //Incrementa el espacio entre línea y línea
+                contador += incremento;
+            } while (contador <= 602);
+            contador -= incremento;
+
+            //Segundo gráfico
+            Lienzo.StrokeColor = Colors.Blue;
+            incremento = 2;
+            do {
+                Lienzo.DrawLine(dirtyRect.Height / 2, contador, dirtyRect.Height, contador);
+                incremento++;
+                contador -= incremento;
+            } while (contador >= 10);
+
+
+        }
+    }
+}
