a751089b80611ac2de2a6b1057105d037274a760
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2026-02-20 19:23:20 -0500

    Add files via upload

---
+++ 03/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Ejemplo {
+    public partial class MainPage : ContentPage {
+
+        public MainPage() {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ 04/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Ejemplo {
+    public partial class MainPage : ContentPage {
+
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void CuandoHaceClic(object sender, EventArgs e) {
+            string Nombre = UnNombre.Text;
+            LaSalida.Text = "Hola " + Nombre;
+        }
+    }
+}
