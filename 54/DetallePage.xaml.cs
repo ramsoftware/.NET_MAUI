99c11b25fe934715b4c9695ca479a760f5347705
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2026-04-01 11:25:57 -0500

    Add files via upload

---
+++ 54/DetallePage.xaml.cs
@@ -0,0 +1,42 @@
+namespace BaseDatos {
+    public partial class DetallePage : ContentPage {
+        private readonly Videojuego Item;
+        private readonly ContextoBaseDatos BaseDatos;
+
+        public DetallePage(Videojuego item) {
+            InitializeComponent();
+
+            // Guardamos el videojuego
+            Item = item;
+
+            // Obtenemos la BD desde Dependency Injection
+            BaseDatos = IPlatformApplication.Current.Services.GetService<ContextoBaseDatos>();
+
+            // Mostrar los datos
+            LblId.Text = item.Id.ToString();
+            LblNombre.Text = item.Nombre;
+            LblPlataforma.Text = item.ObjPlataforma?.Nombre;
+            LblGenero.Text = item.ObjGenero?.Nombre;
+            LblAnyo.Text = item.Anyo.ToString();
+        }
+
+        private async void Borrar_Clicked(object sender, EventArgs e) {
+            bool respuesta = await DisplayAlertAsync(
+                "Confirmar borrado",
+                "¿Usted está seguro de borrar este videojuego?",
+                "Sí",
+                "No"
+            );
+
+            if (!respuesta)
+                return; // No hace nada, solo cierra el aviso.
+
+            // Si presionó "Sí", borramos
+            BaseDatos.Remove(Item);
+            await BaseDatos.SaveChangesAsync();
+
+            // Regresamos al grid
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ 54/Plataforma.cs
@@ -0,0 +1,6 @@
+﻿namespace BaseDatos {
+    public class Plataforma {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+    }
+}
