34aa8b33d6c70bfb8079ff4f371b87b6572e295f
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2026-04-01 00:39:22 -0500

    Add files via upload

---
+++ 50/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BaseDatos {
+
+    public partial class MainPage : ContentPage {
+
+        private readonly ContextoBaseDatos BaseDatos;
+        
+        public MainPage(ContextoBaseDatos BaseDatos) {
+            InitializeComponent();
+            this.BaseDatos = BaseDatos;
+        }
+
+        async private void AlDarClic(object? sender, EventArgs e) {
+            var lista = await BaseDatos.Colores.ToListAsync();
+            Resultado.Text = "";
+            for (int Cont = 0; Cont < lista.Count; Cont++) {
+                Resultado.Text += $"Código: {lista[Cont].Id} - Nombre: {lista[Cont].Nombre}\n";
+            }
+        }
+    }
+}
+    
---
+++ 51/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BaseDatos {
+    public partial class MainPage : ContentPage {
+        private readonly ContextoBaseDatos BaseDatos;
+
+        private List<Colores> Registros = new();
+        private const int TamañoPagina = 10;
+        private int PaginaActual = 0;
+
+        public MainPage(ContextoBaseDatos BaseDatos) {
+            InitializeComponent();
+            this.BaseDatos = BaseDatos;
+            CargarDatos();
+        }
+
+        private async void CargarDatos() {
+            Registros = await BaseDatos.Colores.ToListAsync();
+            MostrarPagina();
+        }
+
+        private void MostrarPagina() {
+            var datos = Registros
+                .Skip(PaginaActual * TamañoPagina)
+                .Take(TamañoPagina)
+                .ToList();
+
+            GridDatos.ItemsSource = datos;
+        }
+
+        private void IrInicio_Clicked(object sender, EventArgs e) {
+            PaginaActual = 0;
+            MostrarPagina();
+        }
+
+        private void Atras_Clicked(object sender, EventArgs e) {
+            if (PaginaActual > 0)
+                PaginaActual--;
+            MostrarPagina();
+        }
+
+        private void Adelante_Clicked(object sender, EventArgs e) {
+            if ((PaginaActual + 1) * TamañoPagina < Registros.Count)
+                PaginaActual++;
+            MostrarPagina();
+        }
+
+        private void IrFinal_Clicked(object sender, EventArgs e) {
+            PaginaActual = (Registros.Count - 1) / TamañoPagina;
+            MostrarPagina();
+        }
+    }
+}
